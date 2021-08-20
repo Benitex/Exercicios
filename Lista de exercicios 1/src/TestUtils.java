b7af01a9c4d9e785fb35d78f3858d20c1ebca37d
Benitex <benitoapepe@yahoo.com.br>
2021-08-20 16:30:11 -0300

    Utils adicionado

---
+++ Lista de exercicios 1/src/TestUtils.java
@@ -0,0 +1,5 @@
+public class TestUtils {
+    public static void main(String[] args) {
+        System.out.println(Utils.calculaMedia(10,1,5));
+    }
+}
---
+++ Lista de exercicios 1/src/Utils.java
@@ -0,0 +1,11 @@
+public class Utils{
+    static double calculaMedia(double x,double y){
+        return (x+y)/2;
+    }
+    static double calculaMedia(double x, double y, double z){
+        return (x+y+z)/3;
+    }
+    static double calculaMedia(double a, double b, double c, double d){
+        return (a+b+c+d)/4;
+    }
+}
