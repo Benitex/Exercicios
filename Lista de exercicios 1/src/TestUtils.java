35a076abee5a1e90b9ea0dca216dd340493696f4
Benito <benitoapepe@yahoo.com.br>
2021-08-31 18:51:24 -0300

    Média de até 4 números adicionada

--- Lista de exercicios 1/src/TestUtils.java
+++ Lista de exercicios 1/src/TestUtils.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class TestUtils {
     public static void main(String[] args) {
-        System.out.println(Utils.calculaMedia(10,1,5));
+        System.out.println("O que gostaria de fazer?");
+        Scanner scanner = new Scanner(System.in);
+        String operacao = scanner.nextLine();
+        if (operacao.equalsIgnoreCase("Bhaskara")){
+
+        }
+        else if (operacao.equalsIgnoreCase("Media")){
+            double n[] = new double[6];
+            int quantidade = 1;
+            while(n[quantidade-1]!=0){
+                if(quantidade<=4){
+                    System.out.println("Insira o "+quantidade+"° número.");
+                    n[quantidade] = scanner.nextDouble();
+                    quantidade++;
+                }else
+                    n[quantidade] = 0;
+            }
+            if (quantidade == 2)
+                Utils.calculaMedia(n[1], n[2]);
+            else if (quantidade == 3)
+                Utils.calculaMedia(n[1], n[2], n[3]);
+            else if (quantidade == 4)
+                Utils.calculaMedia(n[1], n[2], n[3], n[4]);
+        }
+        scanner.close();
     }
 }
--- Lista de exercicios 1/src/Utils.java
+++ Lista de exercicios 1/src/Utils.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class Utils{
     static double calculaMedia(double x,double y){
         return (x+y)/2;
@@ -7,5 +9,14 @@ public class Utils{
     }
     static double calculaMedia(double a, double b, double c, double d){
         return (a+b+c+d)/4;
+    }
+    static void bhaskara(float a, float b, float c) {
+        float delta = ((b*b)-(4*a*c));
+        if (delta<0)
+            System.out.println("Raízes imaginárias.");
+        else if (delta==0)
+            System.out.println("Raiz única = "+(((-b+Math.pow(delta,0.5)))/(2*a)));
+        else
+            System.out.println("Raizes distintas. x1 = "+(((-b+Math.pow(delta,0.5)))/(2*a))+" e x2= "+((-b-Math.pow(delta,0.5)))/(2*a));
     }
 }
