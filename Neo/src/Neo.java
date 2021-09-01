a23e3b989ceb730668099eb42e1f54eec7281f9d
Benito <benitoapepe@yahoo.com.br>
2021-09-01 18:01:16 -0300

    Bug na seleção corrigido

--- Neo/src/Neo.java
+++ Neo/src/Neo.java
@@ -6,10 +6,10 @@ public class Neo {
         Scanner s = new Scanner(System.in);
         do{
             Pokemon p1 = new Pokemon();
-            Neo.definirPokemon(p1);
+            Neo.definirPokemon(p1,s);
             Pokemon p2 = new Pokemon();
-            Neo.definirPokemon(p2);
-            Neo.batalha(p1, p2);
+            Neo.definirPokemon(p2,s);
+            Neo.batalha(p1, p2, s);
             System.out.println("Gostaria de continuar jogado? (s/n)");
             String resposta;
             do{
@@ -27,35 +27,33 @@ public class Neo {
         s.close();
     }
 
-    public static void definirPokemon(Pokemon p){
-        Scanner scanner = new Scanner(System.in);
+    public static void definirPokemon(Pokemon p, Scanner s){
         boolean especieIncluida = false;
 
         while(especieIncluida == false) {
             System.out.println("Escolha o pokemon.");
-            String especie = scanner.next();
+            String especie = s.next();
             if(especie.equalsIgnoreCase("Turtwig")||especie.equalsIgnoreCase("Bulbasaur")){
                 especieIncluida = true;
                 p.setAttributes(especie);
             }else
                 System.out.println(especie+" não é um dos pokemon disponíveis, por favor insira outro.");
         }
-        scanner.close();
     }
 
-    public static void batalha(Pokemon p1, Pokemon p2) {
-        while (p1.getVida()>0||p2.getVida()>0){
+    public static void batalha(Pokemon p1, Pokemon p2, Scanner s) {
+        while (p1.vida>0||p2.vida>0){
             if(p1.getSpeed()>p2.getSpeed()){
-                p1.ataque(p2.getVida());
-                p2.ataque(p1.getVida());
+                p1.ataque(p2.vida,s);
+                p2.ataque(p1.vida,s);
             }else{
-                p2.ataque(p1.getVida());
-                p1.ataque(p2.getVida());
+                p2.ataque(p1.vida,s);
+                p1.ataque(p2.vida,s);
             }
-            System.out.println("Vida de "+p1.getEspecie()+": "+p1.getVida());
-            System.out.println("Vida de "+p2.getEspecie()+": "+p2.getVida());
+            System.out.println("Vida de "+p1.getEspecie()+": "+p1.vida);
+            System.out.println("Vida de "+p2.getEspecie()+": "+p2.vida);
         }
-        if (p2.getVida() < 0)
+        if (p2.vida < 0)
             System.out.println(p2.getEspecie()+" não consegue mais se mover!\n"+p1.getEspecie()+" venceu!");
         else
             System.out.println(p1.getEspecie()+" não consegue mais se mover!\n"+p2.getEspecie()+" venceu!");
--- Neo/src/Pokemon.java
+++ Neo/src/Pokemon.java
@@ -1,27 +1,26 @@
 import java.util.Scanner;
 
 public class Pokemon {
-    private int vida,ataque,speed;
+    public int vida;
+    private int ataque,speed;
     private String especie,golpe[]={"Splash","Splash","Splash","Splash"};
 
     public int getSpeed(){
         return speed;
     }
-    public int getVida(){
-        return vida;
-    }
     public String getEspecie(){
         return especie;
     }
 
-    public void setAttributes(String especie){
-        if(especie.equalsIgnoreCase("Turtwig")){
+    public void setAttributes(String especieEscolhida){
+        especie = especieEscolhida;
+        if(especieEscolhida.equalsIgnoreCase("Turtwig")){
             vida=55;
             ataque=10;
             speed=55;
             golpe[0]="Tackle";
             golpe[1]="Growl";
-        }else if(especie.equalsIgnoreCase("Bulbasaur")){
+        }else if(especieEscolhida.equalsIgnoreCase("Bulbasaur")){
             vida=45;
             ataque=11;
             speed=65;
@@ -30,19 +29,17 @@ public class Pokemon {
         }
     }
 
-    public void ataque (int vida){
+    public void ataque (int vida, Scanner s){
         int golpeEscolhido;
         
-        System.out.println("Escolha o golpe do "+especie+": ");
-        System.out.println("1 - "+golpe[1]+"2 - "+golpe[2]+"3 - "+golpe[3] + "4 - "+golpe[4]);
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escolha o golpe do "+especie+":");
+        System.out.println("1 - "+golpe[0]+"\t2 - "+golpe[1]+"\t3 - "+golpe[2] + "\t4 - "+golpe[3]);
         do{
-            golpeEscolhido = scanner.nextInt()-1;
-            if (golpeEscolhido<1||golpeEscolhido>4)
+            golpeEscolhido = s.nextInt()-1;
+            if (golpeEscolhido<0||golpeEscolhido>3)
                 System.out.println("Escolha um valor entre 1 e 4.");
-        }while(golpeEscolhido<1||golpeEscolhido>4);
-        scanner.close();
-        System.out.println(especie+" utilizou "+golpeEscolhido);
+        }while(golpeEscolhido<0||golpeEscolhido>3);
+        System.out.println(especie+" utilizou "+golpe[golpeEscolhido]);
         if(golpe[golpeEscolhido]=="Tackle")
             vida-=ataque;
     }
